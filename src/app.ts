3175e49961e102ce953b646e108e56484c8a8d2f
jose-farley <jose.farley.sousa@gmail.com>
2024-01-01 13:02:37 -0300

    Adicionando o cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import { routes } from './routes/routes';
 import cors from 'cors'
 
 const app = express();
-app.use(cors)
+app.use(cors())
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(routes);
