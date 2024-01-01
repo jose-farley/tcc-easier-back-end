0eeee24faddf2e2b916b186279a46ac42b8c8c5f
jose-farley <jose.farley.sousa@gmail.com>
2024-01-01 12:51:43 -0300

    Adicionando o cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,10 @@ dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
 import { routes } from './routes/routes';
-
+import cors from 'cors'
 
 const app = express();
+app.use(cors)
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(routes);
